4dc985384a5106cf5c3b1a747ef293dfba674490
vikdas <viks.hardas@gmail.com>
2026-06-20 11:27:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # This code is from https://github.com/LondheShubham153/flask-app-ecs/tree/main
+# this have been copied from londheshubham153 repo
 from flask import Flask, render_template
 app = Flask(__name__)
 
